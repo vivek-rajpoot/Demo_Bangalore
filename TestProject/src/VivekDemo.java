fcd02065815c83febe966d2e1192827891181aef
Vivek.Rajpoot <Vivek.Rajpoot@LT-HICCONS02640.s1.MS.UNILEVER.com>
2019-08-28 21:55:31 +0530

    final commit

--- TestProject/src/VivekDemo.java
+++ TestProject/src/VivekDemo.java
@@ -1,7 +1,22 @@
 
 public class VivekDemo {
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		int[] arr= {10,20,20,30,40,40,50,50};
+		int len=arr.length;
+		RemovedDuplicateInArray(arr,len);
+	}
+
+	private static void RemovedDuplicateInArray(int[] arr, int len) {
+		  int[] temp=new int[len];
+		  int j=0;
+		  for(int i=0;i<len-1;i++) {
+			  if(arr[i]!=arr[i+1]) {
+				  temp[j++]=arr[i];
+			  }
+		  }temp[j++]=arr[len-1];
+		  for (int i = 0; i < j; i++) {
+			System.out.println(temp[i]+" ");
+		}
 	}
 
 }
